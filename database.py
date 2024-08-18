a7850d9bdd57c5a1fd6923349a14a35f7a952d19
GianmarcoIC <i2415363@continental.edu.pe>
2024-08-18 17:13:11 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 import os
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+# Definición de la base de datos
 Base = declarative_base()
 
 class Item(Base):
@@ -10,8 +12,9 @@ class Item(Base):
     nombre = Column(String, nullable=False)
     cantidad = Column(Integer, nullable=False)
 
+# Función para inicializar la base de datos
 def init_db():
-    database_url = os.getenv('postgresql://postgres:ibBPwUDErJYUSAtGTlOjncsqBDZnBFEa@autorack.proxy.rlwy.net:25567/railway')
-    engine = create_engine(database_url)
-    Base.metadata.create_all(engine)
+    database_url = os.getenv('postgresql://postgres:ibBPwUDErJYUSAtGTlOjncsqBDZnBFEa@autorack.proxy.rlwy.net:25567/railway')  # Obtiene la URL de la base de datos desde las variables de entorno
+    engine = create_engine(database_url)  # Crea una instancia del motor de la base de datos
+    Base.metadata.create_all(engine)  # Crea todas las tablas en la base de datos
     return engine
